8ccf54181ff926cf48e74e48231ff77fabe3cb9f
Jordy Lima <jordy.sousa@eng.ci.ufpb.br>
2020-04-09 08:38:39 -0300

    New

---
+++ Básicos/bitwise.java
@@ -0,0 +1,19 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//bitwise
+			//& AND bit a bit
+			//| OR bit a bit
+			//^ XOR bit a bit
+		int n1 = 89, n2 = 60;
+
+		System.out.println(n1 & n2);//0101 1001 AND 0011 1100
+		System.out.println(n1 | n2);//0101 1001 OR  0011 1100
+		System.out.println(n1 ^ n2);//0101 1001 XOR 0011 1100					
+	}
+
+}
