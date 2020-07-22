6cbd5b66e0c6e93e02c7378b32f90a3cc6285b0a
JordyLimaEng <jordy.sousa@eng.ci.ufpb.br>
2020-07-22 18:44:28 -0300

    Update Renomeador.java

--- Renomeador.java
+++ Renomeador.java
@@ -11,30 +11,37 @@ import java.util.Scanner;
 public class Renomeador {
 
 	public static void main(String[] args) {
-		
-				//pega todos os arquivos de um diretório e renomeia para um nome ou sequência requerida
 
-				File dir = new File("C:\\Users\\Jordy\\Documents\\React-Native\\catalogo\\src\\images");//arquivos de origem
+		// pega todos os arquivos de um diretório e renomeia para um nome ou sequência
+		// requerida
 
-				int indice = 0;
-		
-				if (dir.isDirectory()) { // verifica se é diretório
-					for (final File f : dir.listFiles()) {
-						try {
-							indice++;
-							File newfile = new File("C:\\Users\\Jordy\\Documents\\React-Native\\catalogo\\src\\images\\" + indice + ".jpg");//local e arquivos onde vai salvar
-		
-							if (f.renameTo(newfile)) {
-								System.out.println("Arquivo renomeado");
-							} else {
-								System.out.println("Não foi possível renomear");
-							}
-						} catch (Exception e) {
-							e.printStackTrace();
-						}
+		File dir = new File("C:\\Users\\Jordy\\Documents\\React-Native\\catalogo\\src\\images");// arquivos de origem
+
+		int indice = 0;
+
+		if (dir.isDirectory()) { // verifica se é diretório
+			for (final File f : dir.listFiles()) {
+				try {
+					indice++;
+					File newfile = new File(
+							"C:\\Users\\Jordy\\Documents\\React-Native\\catalogo\\src\\images\\" + indice + ".jpg");// local
+																													// e
+																													// arquivos
+																													// onde
+																													// vai
+																													// salvar
+
+					if (f.renameTo(newfile)) {
+						System.out.println("Arquivo renomeado");
+					} else {
+						System.out.println("Não foi possível renomear");
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
 				}
 			}
-				
 		}
-		
+
+	}
+
 }
